ae9781a3c93174af24b4d471143159f73e82a4fd
dmedinas <dmedinas@gmail.com>
2023-07-08 00:33:00 +0200

    Pomodoro: Se agrega el timer para completar tarea

--- POMODORO/main.js
+++ POMODORO/main.js
@@ -10,6 +10,8 @@ let current = null;// Tarea actual que se esta ejecutando
 const bAdd = document.querySelector('#bAdd')
 const itTask = document.querySelector('#itTask')
 const form = document.querySelector('#form')
+const taskName = document.querySelector('#time #taskName')
+const taskContainer = document.querySelector('#tasks')
 
 
 form.addEventListener('submit', e =>{
@@ -42,7 +44,48 @@ function renderTasks(){
         `;
     });
 
-    console.log(html);
-    const taskContainer = document.querySelector('#tasks')
+
     taskContainer.innerHTML = html.join("")//El metodo map va a devolver un arreglo de strings, con el metodo join los unimos todo a un solo string
+
+    const startButtons = document.querySelectorAll('.task .start-button')
+    startButtons.forEach(button =>{
+        button.addEventListener('click', (e) =>{
+            if (!timer){
+                const id = button.getAttribute("data-id");
+                startButtonHandler(id);
+                button.textContent = "In progress ...";
+            }
+        } )
+    })
 }
+
+const startButtonHandler = (id =>{
+    time = 5;
+    current = id;
+    const taskIndex = tasks.findIndex((task) => task.id === id);
+    taskName.textContent = tasks[taskIndex].title;
+    timer = setInterval(() =>{
+        timeHandler(id);
+    }, 1000);//Ejecuta una funcion de forma indefinida hasta que yo la detenga
+})
+
+function timeHandler(id){
+    time--;
+    renderTime()
+    if(time === 0){
+        clearInterval(timer)
+        current = null;
+        taskName.textContent = "";
+        taskContainer.innerHTML = "  ";
+        renderTime();
+    }
+}
+
+function renderTime(){
+    const timeDiv = document.querySelector('#time #value');
+    const minutes = parseInt(time / 60);
+    const seconds = parseInt(time % 60);
+
+    timeDiv.textContent =`${minutes < 10 ? "0" : ""}${minutes} : ${seconds < 10 ? "0" : ""}${seconds}`
+}
+
