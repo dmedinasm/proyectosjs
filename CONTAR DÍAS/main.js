3a493c0b7b0a264bf4731d3dfbf95f90b506eccd
dmedinas <dmedinas@gmail.com>
2023-07-11 23:13:49 +0200

    Agregado mostrar eventos y eliminarlos en proyecto contar dias

--- CONTAR DÍAS/main.js
+++ CONTAR DÍAS/main.js
@@ -5,7 +5,7 @@ const eventName = document.querySelector('#eventName')
 const eventDate = document.querySelector('#eventDate')
 const buttonAdd = document.querySelector('#bAdd')
 
-const eventsContainer = document.querySelector('#eventsContainer')
+const eventsContainer = document.querySelector('.eventsContainer')
 
 document.querySelector('form').addEventListener('submit', e => {
     e.preventDefault();
@@ -40,7 +40,35 @@ function dateDiff(d){
     const targetDate = new Date(d)
     const today = new Date()
 
-    const difference = targetDate.getTime() - dateNow.getTime()
-    const days = Math.ceil(difference/1000*3600*24)
+    const difference = targetDate.getTime() - today.getTime()
+    const days = Math.ceil(difference/(1000*3600*24))
     return days
+}
+
+function renderEvents(){
+    const eventsHtml = events.map(event => {
+        return `
+             <div class="event">
+                   <div class="days">
+                   <span class="days-number">${dateDiff(event.date)}</span>
+                   <span class="days-text">días</span>
+                   </div>
+
+                   <div class="event-name">${event.name}</div>
+                   <div class="event-date">${event.date}</div>
+                   <div class="actions">
+                       <button class="bDelete" data-id="${event.id}">Eliminar</button>
+                   </div>
+             </div>
+        `
+    })
+
+    eventsContainer.innerHTML = eventsHtml.join("")
+    document.querySelectorAll('.bDelete').forEach(button => {
+        button.addEventListener('click', e =>{
+            const id = button.getAttribute('data-id')
+            events = events.filter(event => event.id !== id)
+            renderEvents()
+        })
+    })
 }
