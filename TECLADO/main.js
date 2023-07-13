e5d9199d34a1862b59409807fb251d6111798694
dmedinas <dmedinas@gmail.com>
2023-07-13 23:55:00 +0200

    Proyecto Teclado, proyecto completado

--- TECLADO/main.js
+++ TECLADO/main.js
@@ -60,21 +60,22 @@ const keys = [
 
 let mayus = false 
 let shift = false
+let current = null;// Va a tomar la referencia del ultimo input seleccionado, para cuando le de clic, inserte la letra
 
 renderKeyboard()
 
 function renderKeyboard(){
     const keyboardContainer = document.querySelector('#keyboard-container');
-    let empty =`<div class="key-empty></div>` // Para hacer efecto de espacio vacio
+    let empty =`<div class="key-empty"></div>` // Para hacer efecto de espacio vacio
 
     // Arreglo mutidimensional, el primer map es para el arreglo principal y el segundo map para cada arreglo dentro del principal 
     const layers = keys.map((layer) => {
         return layer.map(key =>{
             if(key[0] === "SHIFT") {
-                return `<button class="key key-shift">${key[0]}</button>`
+                return `<button class="key key-shift ${shift ? 'activated' : ''}">${key[0]}</button>`
             }
             if(key[0] === "MAYUS") {
-                return `<button class="key key-mayus">${key[0]}</button>`
+                return `<button class="key key-mayus ${mayus ? 'activated' : ''}">${key[0]}</button>`
             }
 
             if (key[0] === "SPACE"){
@@ -82,8 +83,8 @@ function renderKeyboard(){
             }
 
             return `
-            <button class="key key-normal">${shift ? key[1] : mayus && key[0].tolowerCase().charCodeAt(0) >= 97 &&
-                                                                       key[0].tolowerCase().charCodeAt(0) <= 122
+            <button class="key key-normal">${shift ? key[1] : mayus && key[0].toLowerCase().charCodeAt(0) >= 97 &&
+                                                                       key[0].toLowerCase().charCodeAt(0) <= 122
                                                                        ? key[1]
                                                                        : key[0]
              }</button>
@@ -92,8 +93,8 @@ function renderKeyboard(){
         })
     })
 
-    //layers[0].push(empty)//
-    //layers[1].unshift(empty)//
+    layers[0].push(empty)
+    layers[1].unshift(empty)
 
     const htmlLayers =  layers.map((layer) => {
         return layer.join("")
@@ -104,4 +105,33 @@ function renderKeyboard(){
     htmlLayers.forEach(layer => {
         keyboardContainer.innerHTML += `<div class="layer">${layer}</div>`
     })
+
+    document.querySelectorAll('.key').forEach(key => {
+        key.addEventListener('click', e =>{
+            if(current) {//Si hay un input que puedo utilizar para mi teclado
+                if(key.textContent === "SHIFT"){
+                    shift = !shift
+                }else if(key.textContent === "MAYUS"){
+                    mayus = !mayus
+                }else if (key.textContent === ""){
+                    current.value += " "
+                }else {
+                    current.value += key.textContent.trim()
+                    if(shift){
+                        shift = false
+                        
+                    }
+                }
+                renderKeyboard()
+                current.focus()
+            }
+            
+        })
+    })
 }
+
+document.querySelectorAll("input").forEach(input => {
+    input.addEventListener('focusin', e =>{
+        current = e.target;//Devuelve el objeto al que se envia el evento, su objetivo
+    })
+})
